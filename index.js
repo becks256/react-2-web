896becd78c97ba4d25d0dd3a7bef06a6e83b4e30
Dan Becker <78490119+daniel-becker-kr@users.noreply.github.com>
2024-01-26 12:18:32 -0500

    realtime testing; will be removed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import "./src/components/KdsButton.js"
 import "./src/components/KdsText.js"
 import "./src/components/KdsToggle.js"
+import "./src/components/KdsTabs.js"
---
+++ src/components/KdsTabs.js
@@ -0,0 +1,4 @@
+import { Tabs } from "@kroger/cx-kds-react";
+import { convertComponent } from "../utils/convertComponent";
+
+convertComponent("kds-tabs", Tabs, { parse: ["tabs"] });
